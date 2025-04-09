7026c6ef047e819349dd1461fd5698e7da858f92
wonpyol <wonpyol@gmail.com>
2025-04-09 13:52:32 +0900

    feat: d-work

--- src/main/java/com/example/StudyGrpcApplication.java
+++ src/main/java/com/example/StudyGrpcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyGrpcApplication {
-
+    //d-work
     public static void main(String[] args) {
         SpringApplication.run(StudyGrpcApplication.class, args);
     }
